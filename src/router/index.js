030c3a1987183d63217ee11b3f67e0084ef74712
kuoshinshin <kuoshinshin@example.com>
2026-02-03 19:07:47 +0800

    修改路由模式为hash模式以支持GitHub Pages部署

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import ProductDetail from '../views/ProductDetail.vue'
 import SampleList from '../views/SampleList.vue'
@@ -23,7 +23,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
